ea5c9dd7a5f77818b865c29dcd20ad101b1ad95a
phette23 <phette23@gmail.com>
2015-09-01 09:59:42 -0700

    has_syllabus: skip over Grad Writing mentored study, too

--- has_syllabus.py
+++ has_syllabus.py
@@ -5,20 +5,20 @@ def has_syllabus (row):
     a course, checks it against several possible attributes of courses that do
     not have syllabi, & returns False if it looks like a non-syllabus course.
     """
-    if row['title'] == 'Independent Study' or row['title'] == 'Graduate Studio Practice':
+    if row['title'] in ('Independent Study', 'Graduate Studio Practice', 'Mentored Study'):
         return False
 
     coursenum = int(row['course'].split('-')[1])
 
     # (3|6)96 -> (Under)grad independent study
     # (3|6)98 -> (Under)grad internship
-    skipped_coursenums = (698, 398, 396, 696)
-
-    if coursenum in skipped_coursenums:
+    if coursenum in (698, 398, 396, 696):
         return False
 
-    # FINAR-660 is Graduate Studio Practice, should be caught above but just in case
-    if row['course'] == 'FINAR-660':
+    # FINAR-660 is Graduate Studio Practice, no syllabus
+    # WRITE-660 is Mentored Study which submits inidividual contracts
+    # both of these should be caught above but just in case
+    if row['course'] in ('FINAR-660', 'WRITE-660'):
         return False
 
     # fallthrough; nothing else fired so it must have a syllabus
