74507ca4862737e7326499d4fa1ddade3432c84c
phette23 <phette23@gmail.com>
2015-09-29 10:46:12 -0700

    report conversion, more elegant script

--- missing-syllabi-report-to-csv.sh
+++ missing-syllabi-report-to-csv.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 # usage:
 # missing-syllabi-report-to-csv.sh equella-report.txt > missing.csv
-REPORT="$1"
-sed -e 's/  :  /","/g' $REPORT > tmp
-cat tmp | while read line; do
+
+# separator used in BIRT report
+SEP='  :  '
+sed -e "s/${SEP}/\",\"/g" "$1" | while read line; do
     echo "\"$line\""
 done;
-rm tmp
