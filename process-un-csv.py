4cea0dd1d1ed4fa2672f6975567beb5765a6cf40
phette23 <phette23@gmail.com>
2016-01-22 09:31:25 -0800

    link to faculty username report

--- process-un-csv.py
+++ process-un-csv.py
@@ -1,5 +1,6 @@
 '''
 One-time script to process export from Faculty Usernames report
+https://vm-informer-01.cca.edu/informer/?locale=en_US#action=ReportDetails&reportId=103645186
 into a Python dict which can then be pasted into usernames.py
 '''
 import csv
@@ -10,8 +11,8 @@ reader = csv.DictReader(csvfile, ['fname', 'lname', 'pname', 'username'])
 out = {}
 
 for row in reader:
-    if row['username'] != ':
-        if row['pname'] != ':
+    if row['username'] != '':
+        if row['pname'] != '':
             out[row['pname']] = row['username']
         else:
             out[row['fname'] + ' ' + row['lname']] = row['username']
