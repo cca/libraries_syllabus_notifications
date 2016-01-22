df9a0f0ed311c05f6351bb09aaa73e957f9f298e
phette23 <phette23@gmail.com>
2016-01-22 09:59:15 -0800

    merge faculty usernames report with usernames.py dict

--- process-un-csv.py
+++ process-un-csv.py
@@ -2,19 +2,25 @@
 One-time script to process export from Faculty Usernames report
 https://vm-informer-01.cca.edu/informer/?locale=en_US#action=ReportDetails&reportId=103645186
 into a Python dict which can then be pasted into usernames.py
+Usage:
+> python process-un-csv.py data/faculty-usernames.csv > usernames.py
 '''
 import csv
+import sys
+from usernames import usernames
 
-# change filename to whatever the report CSV is
-csvfile = open('Faculty Usernames.csv')
+# file name is passed on command line
+csvfile = open(sys.argv[1])
 reader = csv.DictReader(csvfile, ['fname', 'lname', 'pname', 'username'])
-out = {}
+report_usernames = {}
 
 for row in reader:
     if row['username'] != '':
         if row['pname'] != '':
-            out[row['pname']] = row['username']
+            report_usernames[row['pname']] = row['username']
         else:
-            out[row['fname'] + ' ' + row['lname']] = row['username']
+            report_usernames[row['fname'] + ' ' + row['lname']] = row['username']
 
-print(out)
+# merge the report's usernames dict with the previous usernames
+usernames.update(report_usernames)
+print(usernames)
