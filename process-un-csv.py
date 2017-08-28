9475652d195ecc854b7af548630b996a97cbf452
phette23 <phette23@gmail.com>
2017-08-28 14:54:14 -0700

    make username process script work with default Informer output

--- process-un-csv.py
+++ process-un-csv.py
@@ -11,15 +11,15 @@ from usernames import usernames
 
 # file name is passed on command line
 csvfile = open(sys.argv[1])
-reader = csv.DictReader(csvfile, ['fname', 'lname', 'pname', 'username'])
+reader = csv.DictReader(csvfile)
 report_usernames = {}
 
 for row in reader:
-    if row['username'] != '':
-        if row['pname'] != '':
-            report_usernames[row['pname']] = row['username']
+    if row['Username'] != '':
+        if row['Preferred Name'] != '':
+            report_usernames[row['Preferred Name']] = row['Username']
         else:
-            report_usernames[row['fname'] + ' ' + row['lname']] = row['username']
+            report_usernames[row['Givenname'] + ' ' + row['Surname']] = row['Username']
 
 # merge the report's usernames dict with the previous usernames
 usernames.update(report_usernames)
