32a8ca58990cc8cf590de7559fbe856967377711
phette23 <phette23@gmail.com>
2020-06-09 10:13:05 -0700

    update sync script to sync app itself

--- sync.sh
+++ sync.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
+DEST="${HOME}/libraries_syllabus_notifications"
 # pull from server
-rsync -avz dev:~/libraries_syllabus_notifications/data .
+rsync -avz dev:"${DEST}/data" .
 # push to server
-rsync -avz data dev:~/libraries_syllabus_notifications
+rsync -avz data dev:"${DEST}"
+rsync -avz --exclude '*.pyc' --exclude 'app/__pycache__' app dev:"${DEST}"
