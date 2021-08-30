8887276474b5da291001f5a7421f7b5183dfe34a
Eric Phetteplace <phette23@gmail.com>
2021-08-30 10:50:10 -0700

    sync.sh - one fewer call to `rsync`
    saves me one 2FA

--- sync.sh
+++ sync.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 DEST="${HOME}/libraries_syllabus_notifications"
-# @TODO can we combine these 3 rsyncs so I do the SSH 2FA fewer times?
 # pull from server
 rsync -avz dev:"${DEST}/data" .
 # push to server
-rsync -avz data dev:"${DEST}"
-rsync -avz --exclude '*.pyc' --exclude 'app/__pycache__' app dev:"${DEST}"
+rsync -avz --exclude '*.pyc' --exclude 'app/__pycache__' app data dev:"${DEST}"
